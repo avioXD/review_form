f5e2b457e18c2cbc2eeb9be35125aa4773c1040c
ABHISHEK DAS <67656730+avioXD@users.noreply.github.com>
2022-12-15 18:11:14 +0530

    done

--- index.js
+++ index.js
@@ -6,27 +6,18 @@ const app = express();
 const path = require("path");
 // set the view engine to ejs
 app.set("view engine", "ejs");
-
+const dbconfig = {
+  host: "c1256c5a-fa5f-4d4a-afa8-9b05cad748e0.hana.prod-ap10.hanacloud.ondemand.com",
+  port: 443,
+  user: "HACK2BUILD#PYTHON",
+  password: "&zVf@qfQ(2)$Y-M-8/w6xUCDYPz8;Tc.",
+};
 ////
-
 let data = [];
+let all_city = [];
+let all_dealer = [];
 router.get("/", function (req, res) {
-  res.render("index", { data: [] });
-  //__dirname : It will resolve to your project folder.
-});
-router.get("/review/:dealer_id", (req, res) => {
-  console.log(req.params.dealer_id);
-  res.render("review");
-});
-router.get("/getdealer", (req, res) => {
-  const { year } = req.query;
-  console.log(year);
-  var client = hdb.createClient({
-    host: "c1256c5a-fa5f-4d4a-afa8-9b05cad748e0.hana.prod-ap10.hanacloud.ondemand.com",
-    port: 443,
-    user: "HACK2BUILD#PYTHON",
-    password: "&zVf@qfQ(2)$Y-M-8/w6xUCDYPz8;Tc.",
-  });
+  var client = hdb.createClient(dbconfig);
   client.on("error", function (err) {
     console.error("Network connection error", err);
   });
@@ -34,17 +25,69 @@ router.get("/getdealer", (req, res) => {
     if (err) {
       return console.error("Connect error", err);
     }
-    client.exec(
-      `SELECT * FROM "HACK2BUILD"."SAP.TIME.VIEW_DIMENSION_DAY" WHERE YEAR=${year} limit 5`,
-      function (err, rows) {
-        client.end();
-        if (err) {
-          return console.error, err;
+    try {
+      client.exec(
+        `SELECT DISTINCT ORT01 FROM "HACK2BUILD"."V_KNA1"`,
+        function (err, city) {
+          client.end();
+          if (err) {
+            return console.error, err;
+          }
+          console.log("Results:", typeof city, city);
+          all_city = city;
+          res.render("index", { city: city, data: data });
         }
-        console.log("Results:", typeof rows, rows);
-        res.render("index", { data: rows });
-      }
-    );
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  //__dirname : It will resolve to your project folder.
+});
+
+////
+
+router.get("/review/:dealer_id", (req, res) => {
+  let selected_dealer = {};
+  all_dealer.map((d) => {
+    if (d.KUNNR == req.params.dealer_id) {
+      selected_dealer = d;
+    }
+  });
+  res.render("review", { data: selected_dealer });
+});
+router.post("/submit-review");
+/////
+
+router.get("/getdealer", (req, res) => {
+  const { CITY } = req.query;
+  var client = hdb.createClient(dbconfig);
+  client.on("error", function (err) {
+    console.error("Network connection error", err);
+  });
+  client.connect(function (err) {
+    if (err) {
+      return console.error("Connect error", err);
+    }
+    try {
+      client.exec(
+        `SELECT  KUNNR, NAME1,ORT01 FROM "HACK2BUILD"."V_KNA1" WHERE ORT01 = '${CITY}' `,
+        function (err, rows) {
+          client.end();
+          if (err) {
+            return console.error, err;
+          }
+          all_dealer = [...rows];
+          res.render("index", {
+            data: rows,
+            city: all_city,
+            selected_city: CITY,
+          });
+        }
+      );
+    } catch (e) {
+      console.log(e);
+    }
   });
 });
 
